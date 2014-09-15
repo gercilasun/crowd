943e7c4f273b33957647f493cc18c2afab2ee461
gercilasun <gokalp@ercilasun.com>
2014-09-14 21:48:57 -0400

    demo

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -1,11 +1,21 @@
 <?php
 
 
-class PagesController extends BaseController {
+class PagesController extends BaseController
+{
+
     public  function home()
     {
         $name = 'John';
 
         return View::make('hello')->with('name',$name);
     }
+
+    public function about()
+    {
+
+        return View::make('about');
+
+    }
+
 }
